00c63f8d7ae5401d0c52b62c3a9636bd39f5153e
Ian Meintjes <meintjes@umich.edu>
2016-10-08 02:50:08 -0400

    add handling of the non-wobsacket part

--- app.py
+++ app.py
@@ -1,9 +1,28 @@
-from flask import Flask, render_template
-import extensions
-import controllers
-import config
-import os
+from flask import Flask, current_app
+from game import Game
+import requests
+import random
+
+app = Flask(__name__, template_folder='views')
+
+active_games = {}
+
+@app.route('/')
+def index():
+    return current_app.send_static_file('index.html')
+
+@app.route('/create', methods=['POST'])
+def create():
+    gameid = ''
+    while gameid == '' or gameid in active_games:
+        game.append(c)
+    active_games[gameid] = Game()
+    return id
+
+@app.route('/<path:path>')
+def game_page(path):
+    return current_app.send_static_file('game.html')
 
 if __name__ == '__main__':
     # listen on external IPs
-    app.run(host=config.env['host'], port=config.env['port'], debug=True)
+    app.run(host='localhost', port=80, debug=True)
